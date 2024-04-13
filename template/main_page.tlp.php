edc2eeb328fbbfc381cb5e61e7a3d84efaf2ec0d
Process-ing <rbruno.p.o@gmail.com>
2024-04-13 19:26:34 +0100

    Create start now button

--- template/main_page.tlp.php
+++ template/main_page.tlp.php
@@ -4,5 +4,10 @@
     <div id="welcome-banner">
         <h1>Welcome to Trinted!</h1>
         <p>Buy and sell <strong>trillions of pre-loved</strong> items!</p>
+        <?php drawStartNowButton(); ?>
     </div>
+<?php } ?>
+
+<?php function drawStartNowButton() { ?>
+    <button id="start-now-button">Start Now</button>
 <?php } ?>
