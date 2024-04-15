ea9dc5c2042e22add73f75d7f8c4cb26c7783bc4
Process-ing <rbruno.p.o@gmail.com>
2024-04-15 14:22:27 +0100

    Create like button

--- template/product.tlp.php
+++ template/product.tlp.php
@@ -1,9 +1,16 @@
 <?php declare(strict_types = 1); ?>
 
+<?php function drawLikeButton(string $product) { ?>
+    <button type="checkbox">
+        <span class="material-icons">favorite_border</span>
+    </button>
+<?php } ?>
+
 <?php function drawProductCard(string $product) { ?>  <!-- TODO: Use real database product -->
     <div class="product-card">
         <img src="https://picsum.photos/seed/<?=$product?>/200/300" alt="<?=$product?>">
         <h1><?=$product?></h1>
         <p>$55.49</p>
+        <?php drawLikeButton($product); ?>
     </div>
 <?php } ?>
