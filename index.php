f80c9869b12da175f78128830124b3ec79384be0
racoelhosilva <racoelhosilva@gmail.com>
2024-04-20 17:13:23 +0100

    simple page routing implemented

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
 require_once __DIR__ . '/utils/Autoload.php';
-include_once('pages/main_page.php');
+
+$routes = [
+    '/' => 'Controller@index',
+    '/login' => 'Controller@login',
+];
+
+$request_uri = $_SERVER['REQUEST_URI'];
+$path = parse_url($request_uri, PHP_URL_PATH);
+
+$route = $routes[$path] ?? null;
+
+if ($route) {
+    
+    list($controllerName, $actionName) = explode('@', $route);
+    $controller = new $controllerName();
+    $controller->$actionName();
+
+} else {
+    header("HTTP/1.0 404 Not Found");
+    echo '404 - Page Not Found';
+}
+
 ?>
---
+++ utils/Controller.php
@@ -0,0 +1,11 @@
+<?php
+class Controller {
+
+    public function index() {
+        include_once('pages/main_page.php');
+    }
+
+    public function login() {
+        include_once('pages/login_page.php');
+    }
+}
