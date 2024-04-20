044c04d8b3ac517d1c1b2e2b8de3a06a247f9be5
Process-ing <rbruno.p.o@gmail.com>
2024-04-20 19:09:28 +0100

    Make initial pagination

--- pages/search_page.php
+++ pages/search_page.php
@@ -4,9 +4,19 @@ include_once('template/common.tlp.php');
 include_once('template/search_page.tlp.php');
 ?>
 
+<!-- TODO: Remove this function -->
+<?php function generateProducts(int $from, int $to): array {
+    $products = array();
+    for ($i = $from; $i <= $to; $i++)
+        $products[] = (string) $i;
+    return $products;
+} ?>
+
 <?php function drawSearchPageContent() { ?>
+    <?php $page = (int) $_GET['page'] ?>
     <main id="search-page">
-        <?php drawProducts(); ?>
+        <?php drawProducts(generateProducts($page * 20 - 19, $page * 20)); ?>
+        <?php drawPagination(10, $page); ?>
     </main>
 <?php } ?>
 
--- template/search_page.tlp.php
+++ template/search_page.tlp.php
@@ -4,12 +4,34 @@ declare(strict_types=1);
 include_once('template/product.tlp.php');
 ?>
 
-<?php function drawProducts() { ?>
+<?php function drawProducts(array $products) { ?>
     <div id="searched-products">
         <?php
-        for ($i = 0; $i < 10; $i++) {
-            drawProductCard((string)$i);
+        foreach ($products as $product) {
+            drawProductCard((string)$product);
         }
         ?>
+    </div>
+<?php } ?>
+
+<?php function drawPagination(int $pages, int $current) { ?>
+    <div id="pagination">
+        <?php if ($current > 1) { ?>
+            <a href="?page=<?= $current - 1 ?>">&lt;</a>
+        <?php } else { ?>
+            <a class="blocked">&lt;</a>
+        <?php } ?>
+        <?php for ($i = 1; $i <= $pages; $i++) { ?>
+            <?php if ($i === $current) { ?>
+                <a href="" onclick="return false;" class="active"><?= $i ?></a>
+            <?php } else { ?>
+                <a href="?page=<?= $i ?>"><?= $i ?></a>
+            <?php } ?>
+        <?php } ?>
+        <?php if ($current < $pages) { ?>
+            <a href="?page=<?= $current + 1 ?>">&gt;</a>
+        <?php } else { ?>
+            <a class="blocked">&gt;</a>
+        <?php } ?>
     </div>
 <?php } ?>
