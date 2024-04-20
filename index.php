47c658ae5acceb114316cb2cb9418bef7d8c94e5
racoelhosilva <racoelhosilva@gmail.com>
2024-04-20 17:04:04 +0100

    autoloading implemented

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>Trinted</title>
-</head>
-<body>
-    <?php
-        include_once('pages/main_page.php');
-    ?>
-</body>
-</html>
+<?php
+require_once __DIR__ . '/utils/Autoload.php';
+include_once('pages/main_page.php');
+?>
