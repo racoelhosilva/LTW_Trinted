afe10d52d073fc2b95072107f0d9b649f54936a2
Process-ing <rbruno.p.o@gmail.com>
2024-04-22 16:00:20 +0100

    Add slideshow badges

--- template/product_page.tpl.php
+++ template/product_page.tpl.php
@@ -4,6 +4,10 @@
     <div id="product-photos">
         <span class="material-icons" id="prev-photo">navigate_before</span>
         <span class="material-icons" id="next-photo">navigate_next</span>
+        <div id="photo-badges">
+            <span class="material-icons photo-badge">directions_walk</span>
+            <span class="material-icons photo-badge">directions_run</span>
+        </div>
         <img src="https://picsum.photos/seed/<?=$product_id?>/200/300" alt="Product Photo">
     </div>
 <?php } ?>
