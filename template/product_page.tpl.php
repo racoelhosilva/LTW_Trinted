2a4fea231d27996619daee7567aa68f72f3470c8
Process-ing <rbruno.p.o@gmail.com>
2024-04-22 16:58:35 +0100

    Complete product info

--- template/product_page.tpl.php
+++ template/product_page.tpl.php
@@ -14,6 +14,16 @@
 
 <?php function drawProductInfo(string $product_id) { ?>
     <div id="product-info">
+        <h2>Published two weeks ago</h2>
+        <h2>By <a href="profile">John Doe</a></h2>
         <h1><?= $product_id ?></h1>
+        <p><strong>Size:</strong> L<p>
+        <p><strong>Condition:</strong> Barely Used</p>
+        <p><strong>Category:</strong> Clothing</p>
+        <p><strong>Brand:</strong> Abibas</p>
+        <br>
+
+        <p><strong>Description</strong></p>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam  facilisis sem aliquam tellus dignissim rutrum. Morbi magna erat,  pharetra eu arcu id, elementum.</p>
     </div>
 <?php } ?>
