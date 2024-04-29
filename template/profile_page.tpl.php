00819cc10548083a131511979dbc80e8beeed2fb
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-04-29 15:44:41 +0100

    Added profile picture, name and join date

--- template/profile_page.tpl.php
+++ template/profile_page.tpl.php
@@ -1,12 +1,29 @@
 <?php include_once('template/product.tpl.php'); ?>
 
-<?php function drawProfileImage(String $url) { ?>
-    <img src="https://picsum.photos/seed/<?=$url?>/200/300" class="profile-image" alt="Profile Picture">
+<?php function drawProfileImage(String $url)
+{ ?>
+    <img src="<?= $url ?>" class="profile-image" alt="Profile Picture">
 <?php } ?>
 
-<?php function drawUserInfo() { ?>
+<?php function drawUserInfo(User $user)
+{ ?>
     <div id="user-info">
-        <h1>John Doe</h1>
-        <h2>Joined 4 years ago</h2>
+        <h1><?= $user->name ?></h1>
+        <?php
+        $currentDate = new DateTime(date('Y-m-d', time()));
+        $joinDate = new DateTime(date('Y-m-d', $user->registerDateTime));
+        $timeDifference = $joinDate->diff($currentDate)->y;
+        if ($timeDifference == 0) {
+        ?> <h2>Joined less than a year ago</h2>
+        <?php
+        } elseif ($timeDifference == 1) {
+        ?><h2>Joined one year ago</h2>
+        <?php
+        } else {
+        ?> <h2>Joined <?= $timeDifference ?> years ago</h2>
+        <?php
+        }
+        ?>
+
     </div>
 <?php } ?>
