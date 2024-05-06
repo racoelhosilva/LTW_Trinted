747962699d8d9933708fbc727f2b6be7d67109ff
racoelhosilva <racoelhosilva@gmail.com>
2024-05-06 16:09:02 +0100

    added account settings

--- pages/settings_page.php
+++ pages/settings_page.php
@@ -8,6 +8,19 @@ include_once('template/common.tpl.php');
     <main id="settings-page">
         <section id="account-settings">
             <h2>Account Information</h2>
+            <div class="information-field">
+                <h3>Change Username</h3>
+                <input type="text" id="username" name="username" placeholder="Username">
+            </div>
+            <div class="information-field">
+                <h3>Change E-mail</h3>
+                <input type="text" id="email" name="email" placeholder="E-mail">
+            </div>
+            <div class="information-field">
+                <h3>Change Password</h3>
+                <input type="password" id="password" name="password" placeholder="Password">
+            </div>
+            <input type="submit" value="Save">
         </section>
     </main> 
 <?php } ?>
