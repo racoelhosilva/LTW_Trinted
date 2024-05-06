cab8f19cbb94d05d7e969c8dd8fe097cfdd99d62
Process-ing <rbruno.p.o@gmail.com>
2024-05-06 13:06:06 +0100

    Place more info on order item card

--- template/checkout_page.tpl.php
+++ template/checkout_page.tpl.php
@@ -20,7 +20,11 @@ include_once('db/classes/Post.class.php');
         <img src="<?= $post->getAllImages($db)[0]->url ?>" alt="Product Image">
         <div class="order-item-info">
             <h1><?= $post->title ?></h1>
-            <p>$<?= $post->price ?></p>
+            <p><?= $post->item->size->size ?> - <?= $post->item->condition->condition ?></p>
+        </div>
+        <div class="order-item-info">
+            <p>$<?= $post->price ?></h1>
+            <p class="num-items">2</p>
         </div>
     </div>
 <?php } ?>
