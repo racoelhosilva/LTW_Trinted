8d1aeb5952a44ce4b4f328ee1c5d8ace2c063322
Process-ing <rbruno.p.o@gmail.com>
2024-05-09 21:13:49 +0100

    Create get cart action

---
+++ actions/action_get_cart.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+include_once(__DIR__ . '/../db/classes/Post.class.php');
+
+session_start();
+
+function validate($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+echo json_encode(array('items' => $_SESSION['items']));
